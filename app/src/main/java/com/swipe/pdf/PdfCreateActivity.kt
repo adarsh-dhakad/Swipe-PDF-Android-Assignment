73845b5b0b5ad0b9e4673dc9fe08cbbcbbb321c2
Adarsh dhakad <58937745+adarsh-dhakad@users.noreply.github.com>
2022-08-29 23:32:28 +0530

    string added

--- app/src/main/java/com/swipe/pdf/PdfCreateActivity.kt
+++ app/src/main/java/com/swipe/pdf/PdfCreateActivity.kt
@@ -1,5 +1,6 @@
 package com.swipe.pdf
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.pdf.PdfDocument
@@ -8,11 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.lifecycle.lifecycleScope
 import com.swipe.pdf.databinding.ActivityPdfCreateBinding
+import com.swipe.pdf.databinding.PdfLayoutBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -21,45 +25,52 @@ import java.io.FileOutputStream
 import java.io.IOException
 
 class PdfCreateActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityPdfCreateBinding
+    private lateinit var binding: ActivityPdfCreateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPdfCreateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
+
         binding.btnSave.setOnClickListener {
-            Thread.sleep(1000)
+            //    val inflater = LayoutInflater.from(this@PdfCreateActivity)
+
+//            val inflater: LayoutInflater =
+//                this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//            val view = inflater.inflate(R.layout.pdf_layout, null)
+//            val binding: PdfLayoutBinding = PdfLayoutBinding.inflate(
+//                LayoutInflater.from(this), null, false
+//            )
             lifecycleScope.launch {
-                savePdf()
+                savePdf(this@PdfCreateActivity.binding.idPdfLayout.root)
             }
         }
     }
 
-    private suspend fun savePdf() {
+    private suspend fun savePdf(view: View) {
+
+        //Fetch the dimensions of the viewport
+        val displayMetrics = DisplayMetrics()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            this@PdfCreateActivity.display?.getRealMetrics(displayMetrics)
+            displayMetrics.densityDpi
+        } else {
+            this@PdfCreateActivity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        }
+        view.measure(
+            View.MeasureSpec.makeMeasureSpec(
+                displayMetrics.widthPixels, View.MeasureSpec.EXACTLY
+            ),
+            View.MeasureSpec.makeMeasureSpec(
+                displayMetrics.heightPixels, View.MeasureSpec.EXACTLY
+            )
+        )
+
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels)
 
         withContext(Dispatchers.IO) {
-            val inflater = LayoutInflater.from(this@PdfCreateActivity)
-            val view = inflater.inflate(R.layout.pdf_layout, null)
 
-            //Fetch the dimensions of the viewport
-            val displayMetrics = DisplayMetrics()
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                this@PdfCreateActivity.display?.getRealMetrics(displayMetrics)
-                displayMetrics.densityDpi
-            } else {
-                this@PdfCreateActivity.windowManager.defaultDisplay.getMetrics(displayMetrics)
-            }
-            view.measure(
-                View.MeasureSpec.makeMeasureSpec(
-                    displayMetrics.widthPixels, View.MeasureSpec.EXACTLY
-                ),
-                View.MeasureSpec.makeMeasureSpec(
-                    displayMetrics.heightPixels, View.MeasureSpec.EXACTLY
-                )
-            )
-
-            view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels)
             val bitmap = Bitmap.createBitmap(
                 view.measuredWidth,
                 view.measuredHeight,
@@ -89,7 +100,7 @@ class PdfCreateActivity : AppCompatActivity() {
 
             val file = File(
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                "myBill2.pdf"
+                "myBill.pdf"
             )
             try {
                 pdfDocument.writeTo(FileOutputStream(file))
